6443998745736f84b2898cf9649a59a23ff163dd
yuezhik <1443404546@qq.com>
2022-09-28 12:52:32 +0800

    project update

---
+++ detect_acl/src/main/java/com/rongxin/detectacl/DetectAclApplication.java
@@ -0,0 +1,11 @@
+package com.rongxin.detectacl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DetectAclApplication {
+        public static void main(String[] args) {
+            SpringApplication.run(DetectAclApplication.class, args);
+        }
+}
